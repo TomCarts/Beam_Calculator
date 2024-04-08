f94d67a9d0eb1abb7e8733e598f2182c3ae1e038
TomCarts <tom.cartigny@live.co.uk>
2024-04-08 14:34:46 +0100

    Added material input

--- app.py
+++ app.py
@@ -10,17 +10,12 @@ from PIL import Image
 
 def beam_analysis(nodes_df,members_df,point_loads_df):
 
-
     # Create a new finite element model
     beam = FEModel3D()
-
-    # Define a material
-    E = 29000       # Modulus of elasticity (ksi)
-    G = 11200       # Shear modulus of elasticity (ksi)
-    nu = 0.3        # Poisson's ratio
-    rho = 2.836e-4  # Density (kci)
-    beam.add_material('Material', E, G, nu, rho)
-
+    
+    #Add material
+    for i in range(len(material_df)):
+        beam.add_material(material_df.iloc[i,0],material_df.iloc[i,1],material_df.iloc[i,2],material_df.iloc[i,3],material_df.iloc[i,4])
     
     # Add nodes 
     for i in range(len(nodes_df)):
@@ -73,12 +68,12 @@ def results_table(beam,members_df,nodes_df):
     results_df['Mz']=Mz
     results_df['Fy']=Fy
     results_df['dy']=dy
-    results_df['beam']=0
-    
-    
+    results_df['beam']=0  
 
     return results_df
     
+#Input Data on start up
+
 #Table Data for nodes
 node_1 = {'name': 'Node_A', 'x': 0,'y':0,'z':0,'R_Fx':True,'R_Fy':True,'R_Fz':True,'R_Mx':True,'R_My':False,'R_Mz':False}
 node_2 = {'name': 'Node_B', 'x': 5,'y':0,'z':0,'R_Fx':True,'R_Fy':True,'R_Fz':True,'R_Mx':False,'R_My':False,'R_Mz':False}
@@ -90,7 +85,10 @@ nodes_df.loc[len(nodes_df)] = node_2
 nodes_df.loc[len(nodes_df)] = node_3
 nodes_df.loc[len(nodes_df)] = node_4
 
-
+#Material Table
+material_1 = {'Material':'Material','Stiffness E': 210000, 'Shear Modulus G':75000,'Poission ratio':0.3,'Density':75}
+material_df = pd.DataFrame(columns=['Material','Stiffness E','Shear Modulus G','Poission ratio','Density'])
+material_df.loc[len(material_df)] = material_1
 
 #Beam Table
 member_1 = {'Name': 'M1', 'Node_LHS':'Node_A','Node_RHS':'Node_B','Material':'Material','Iy':0.01,'Iz':0.01,'J':0.1,'A':1,}
@@ -101,54 +99,51 @@ members_df.loc[len(members_df)] = member_1
 members_df.loc[len(members_df)] = member_2
 members_df.loc[len(members_df)] = member_3
 
-
 #Member loads table
 #Point Load Table
-load_1 = {'Member': 'M1', 'Load':'Fy','Value':-5,'Location':2.5}
-load_2 = {'Member': 'M2', 'Load':'Fy','Value':-5,'Location':2.5}
-load_3 = {'Member': 'M3', 'Load':'Fy','Value':-5,'Location':2.5}
-point_loads_df = pd.DataFrame(columns=['Member','Load','Value','Location',])
+load_1 = {'Member': 'M1', 'Load Type':'Fy','Value':-5,'x1':2.5}
+load_2 = {'Member': 'M2', 'Load Type':'Fy','Value':-5,'x1':2.5}
+load_3 = {'Member': 'M3', 'Load Type':'Fy','Value':-5,'x1':2.5}
+point_loads_df = pd.DataFrame(columns=['Member','Load Type','Value','x1',])
 point_loads_df.loc[len(point_loads_df)] = load_1
 point_loads_df.loc[len(point_loads_df)] = load_2
 point_loads_df.loc[len(point_loads_df)] = load_3
 
 
 #UDL Load Table
-load_1 = {'Member': 'M1', 'Load':'Fy','w1':-5,'w2':-5,'x1':0,'x2':5}
-load_2 = {'Member': 'M2', 'Load':'Fy','w1':-5,'w2':-5,'x1':0,'x2':5}
-load_3 = {'Member': 'M3', 'Load':'Fy','w1':-5,'w2':-5,'x1':0,'x2':5}
-udl_loads_df = pd.DataFrame(columns=['Member','Load','w1','w2','x1','x2'])
+load_1 = {'Member': 'M1', 'Load Type':'Fy','w1':-5,'w2':-5,'x1':0,'x2':5}
+load_2 = {'Member': 'M2', 'Load Type':'Fy','w1':-5,'w2':-5,'x1':0,'x2':5}
+load_3 = {'Member': 'M3', 'Load Type':'Fy','w1':-5,'w2':-5,'x1':0,'x2':5}
+udl_loads_df = pd.DataFrame(columns=['Member','Load Type','w1','w2','x1','x2'])
 udl_loads_df.loc[len(udl_loads_df)] = load_1
 udl_loads_df.loc[len(udl_loads_df)] = load_2
 udl_loads_df.loc[len(udl_loads_df)] = load_3
-
-
 
 #Page config
 #image
 image=Image.open('images/sign_convention.PNG')
 
-st.set_page_config(page_title="Civils Optimisation",page_icon=":computer",layout="wide")
+#sidebar
+st.set_page_config(page_title="Beam Calculator",page_icon=":computer",layout="wide")
 with st.sidebar:
     st.header('Infomation')
-    st.write(st.__version__)
     st.subheader('About')
-    st.write('This calculator is run on Pynite FE3Dmodel(). Ensure Nodes match members before calculating results.')
-    st.subheader('Notes')
-    st.write('When adding element to tables ensure the index values are correct and run from 0.')
-    st.subheader('Loads')
-    st.write('Fy = Vertical Point Loads')
-    st.write('Mz = Moment about z-axis')
-    st.write('w1 & w2 = udl magnitude at LHS & RHS respectivly')
-    st.write('UDL Loads: x1 & x2 = udl start and finish point on respective members')
+    st.write('This beam calculator has been developed for educational purposes only and any results should be independently verified.')
+    st.write('The calculation are undertaken using external library Pynite. See documentation [here](https://pynite.readthedocs.io/en/latest/index.html)')
     st.subheader('Sign convention')
     st.image(image)
+    st.subheader('Notes')
+    st.write('When adding element to tables ensure the index added/updated. The index for each table should run from 0 andf increase sequentially i.e. 0,1,2')
+    st.write('Units are defined by user i.e. outputs units are respective to inputs')
     st.subheader('Next Steps')
     st.write("Improve plots to show nodes/supports.")
-    st.write("Add deflection plots.")
+    st.write("Add moving loads option.")
+    st.write("Add option to print as report")
     st.subheader('Author')
     st.write("Tom Cartigny")
-        
+    st.write("Git Hub: [@TomCarts/Beam/Calculator](https://github.com/TomCarts/Beam_Calculator.git)")
+
+#Main Page        
 st.title("Beam Force Diagram Calculator")
 st.header("Inputs")
 st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -158,16 +153,26 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader("Nodes / Restraints")
     nodes_df=st.data_editor(nodes_df, hide_index=True, num_rows="dynamic",column_config={'y':None,'z':None})
-    #hide_index=True, column_config={"B": None}
+    st.write('R_Fx/y/z - Tick to fix translation in corresponding local axis')
+    st.write('R_Mx/y/z - Tick to fix rotation in corresponding local axis')
+    st.subheader("Material")
+    material_df=st.data_editor(material_df, hide_index=True, num_rows="dynamic")
     st.subheader("Members")
     members_df=st.data_editor(members_df,hide_index=True, num_rows="dynamic", column_config={'J':None})
+    st.write('Data must match nodes and materials references')
 
 with col2:
     st.subheader("Loads")
     st.subheader("Point Loads")
     point_loads_df=st.data_editor(point_loads_df, hide_index=True, num_rows="dynamic")
+    st.write('Value - Magnitude of Load')
+    st.write('Load Type: Fy - Vertical Point Loads, Mz - Moment about major axis')
+    st.write('x1 - Location of point load respective to start of member i.e. from left hand node')
     st.subheader("UDL Loads")
     udl_loads_df=st.data_editor(udl_loads_df, hide_index=True, num_rows="dynamic")
+    st.write('Load Type: Fy - Vertical Point Loads, Mz - Moment about major axis')
+    st.write('w1 & w2 - udl magnitude at LHS & RHS respectivly')
+    st.write('x1 & x2 - udl start and finish point respective to start of member i.e. from left hand node')
 
 if st.button("Calculate"):
     # Perform beam analysis
@@ -188,6 +193,4 @@ if st.button("Calculate"):
         st.line_chart(df, x='y', y=['beam','Fy'])
     st.subheader('Deflection Plot')
     st.line_chart(df, x='y', y=['beam','dy'])
-    
-import os
-print(os.path.exists('images/sign_convention.png'))
+
